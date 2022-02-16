19e5344a16837a158c6afc1e4cbd2ded9f96e00e
Durval Henrique <durvalhenriquesouza93@gmail.com>
2022-02-16 00:35:00 -0300

    aguardando thumb add

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ client.on("messageCreate", (msg) => {
     let argsSlice = args.slice(1, 1000).join(" ");
     if (argsSlice.length === 0) {
       msg.reply({
-        content: `Ops! Parece que você esqueceu dos argumentos 😬`,
+        content: `Ops! Parece que você esqueceu de inserir argumentos 😬`,
       });
       if (argsSlice.length === 1) {
         msg.reply({
-          content: `Ops! Parece que você esqueceu do nome do artigo 😬`,
+          content: `Ops! Parece que você esqueceu de inserir o nome do artigo 😬`,
         });
       }
     } else {
       msg.reply({
-        content: `${msg.author} acabou  de mandar o artigo "**${argsSlice}**" para a fila de revisão 🚀`,
+        content: `${msg.author} acabou  de mandar o artigo "**${argsSlice}**" para a fila de revisão 🚀 \n\n Divirta-se, <@257316997707071491> 😁`,
       });
     }
   }
@@ -32,8 +32,6 @@ client.on("messageCreate", (msg) => {
   if (msg.content.startsWith(prefixEditor)) {
     let args = msg.content.split(" ");
     let argsSlice = args.slice(2, 1000).join(" ");
-    console.log(args);
-    console.log(args[1]);
 
     switch (args[1]) {
       case "1":
@@ -46,9 +44,19 @@ client.on("messageCreate", (msg) => {
           content: `Revisão do artigo "**${argsSlice}**" liberada com observações 👀`,
         });
         break;
+      case "1t":
+        msg.reply({
+          content: `Revisão do artigo "**${argsSlice}**" liberada ✅ \n\nAguardando thumb 🖼`,
+        });
+        break;
+      case "2t":
+        msg.reply({
+          content: `Revisão do artigo "**${argsSlice}**" liberada com observações 👀 \n\nAguardando thumb 🖼`,
+        });
+        break;
       default:
         msg.reply({
-          content: "Ops! Argumento inválido 😬",
+          content: "Ops! Parece que você esqueceu de inserir argumentos 😬",
         });
     }
   }
